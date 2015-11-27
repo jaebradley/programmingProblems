f47f9549347edddc5c00cd327040bab0041ace37
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-26 21:31:23 -0500

    reverse word implementation

--- src/main/java/problems/impl/ReverseWordsInStringImpl.java
+++ src/main/java/problems/impl/ReverseWordsInStringImpl.java
@@ -2,32 +2,14 @@ package main.java.problems.impl;
 
 import main.java.problems.interfaces.ReverseWordsInString;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ReverseWordsInStringImpl implements ReverseWordsInString {
-    public String slice_start(String s, int startIndex) {
-        if (startIndex < 0) startIndex = s.length() + startIndex;
-        return s.substring(startIndex);
-    }
-
-    public String slice_end(String s, int endIndex) {
-        if (endIndex < 0) endIndex = s.length() + endIndex;
-        return s.substring(0, endIndex);
-    }
-
-    public String slice_range(String s, int startIndex, int endIndex) {
-        if (startIndex < 0) startIndex = s.length() + startIndex;
-        if (endIndex < 0) endIndex = s.length() + endIndex;
-        return s.substring(startIndex, endIndex);
-    }
     @Override
     public String reverseWordsInString(final String string) {
-        final List<String> reversedWordsStringStore = new ArrayList<>();
-        reversedWordsStringStore.add(0, string);
+        final String[] lastString = new String[1];
+        lastString[0] = string;
+
         int characterIndex = 0;
         int wordIndexStart = 0;
-        int stringStoreIndex = 1;
         final StringBuilder stringBuilder = new StringBuilder();
         while (characterIndex < string.length()) {
             if (' ' != string.charAt(characterIndex)) {
@@ -39,27 +21,25 @@ public class ReverseWordsInStringImpl implements ReverseWordsInString {
 
             if (' ' == string.charAt(characterIndex)) {
                 if (stringBuilder.length() > 0) {
-                    reversedWordsStringStore.add(stringStoreIndex, new StringBuilder().
-                            append(reversedWordsStringStore.get(stringStoreIndex - 1).substring(0, wordIndexStart)).
+                    lastString[0] = new StringBuilder().
+                            append(lastString[0].substring(0, wordIndexStart)).
                             append(stringBuilder.reverse().toString()).
-                            append(reversedWordsStringStore.get(stringStoreIndex - 1).substring(characterIndex)).toString());
+                            append(lastString[0].substring(characterIndex)).toString();
                     stringBuilder.setLength(0);
-                    stringStoreIndex++;
                 }
             }
 
             if (characterIndex == string.length() - 1) {
                 if (stringBuilder.length() > 0) {
-                    reversedWordsStringStore.add(stringStoreIndex, new StringBuilder().
-                            append(reversedWordsStringStore.get(stringStoreIndex - 1).substring(0, wordIndexStart)).
-                            append(stringBuilder.reverse().toString()).toString());
+                    lastString[0] = new StringBuilder().
+                            append(lastString[0].substring(0, wordIndexStart)).
+                            append(stringBuilder.reverse().toString()).toString();
                     stringBuilder.setLength(0);
-                    stringStoreIndex++;
                 }
             }
             characterIndex++;
         }
 
-        return reversedWordsStringStore.get(reversedWordsStringStore.size() - 1);
+        return lastString[0];
     }
 }
--- test/main/java/problems/impl/ReverseWordsInStringImplTest.java
+++ test/main/java/problems/impl/ReverseWordsInStringImplTest.java
@@ -14,4 +14,12 @@ public class ReverseWordsInStringImplTest {
         Assert.assertEquals(expectedReverseString, result);
     }
 
+    @Test
+    public void testExpectedWithSpaces() {
+        final String testString = " a fox   jumped      over a log  ";
+        final String expectedReverseString = " a xof   depmuj      revo a gol  ";
+        final String result = reverseWordsInString.reverseWordsInString(testString);
+        Assert.assertEquals(expectedReverseString, result);
+    }
+
 }
