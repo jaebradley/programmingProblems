17e43e239ae5428869c817716a6d5f7c08267d08
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-26 21:21:49 -0500

    reverse words in string implementation

--- src/main/java/problems/impl/ReverseWordsInStringImpl.java
+++ src/main/java/problems/impl/ReverseWordsInStringImpl.java
@@ -2,9 +2,64 @@ package main.java.problems.impl;
 
 import main.java.problems.interfaces.ReverseWordsInString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReverseWordsInStringImpl implements ReverseWordsInString {
+    public String slice_start(String s, int startIndex) {
+        if (startIndex < 0) startIndex = s.length() + startIndex;
+        return s.substring(startIndex);
+    }
+
+    public String slice_end(String s, int endIndex) {
+        if (endIndex < 0) endIndex = s.length() + endIndex;
+        return s.substring(0, endIndex);
+    }
+
+    public String slice_range(String s, int startIndex, int endIndex) {
+        if (startIndex < 0) startIndex = s.length() + startIndex;
+        if (endIndex < 0) endIndex = s.length() + endIndex;
+        return s.substring(startIndex, endIndex);
+    }
     @Override
     public String reverseWordsInString(final String string) {
-        return null;
+        final List<String> reversedWordsStringStore = new ArrayList<>();
+        reversedWordsStringStore.add(0, string);
+        int characterIndex = 0;
+        int wordIndexStart = 0;
+        int stringStoreIndex = 1;
+        final StringBuilder stringBuilder = new StringBuilder();
+        while (characterIndex < string.length()) {
+            if (' ' != string.charAt(characterIndex)) {
+                if (stringBuilder.length() == 0) {
+                    wordIndexStart = characterIndex;
+                }
+                stringBuilder.append(string.charAt(characterIndex));
+            }
+
+            if (' ' == string.charAt(characterIndex)) {
+                if (stringBuilder.length() > 0) {
+                    reversedWordsStringStore.add(stringStoreIndex, new StringBuilder().
+                            append(reversedWordsStringStore.get(stringStoreIndex - 1).substring(0, wordIndexStart)).
+                            append(stringBuilder.reverse().toString()).
+                            append(reversedWordsStringStore.get(stringStoreIndex - 1).substring(characterIndex)).toString());
+                    stringBuilder.setLength(0);
+                    stringStoreIndex++;
+                }
+            }
+
+            if (characterIndex == string.length() - 1) {
+                if (stringBuilder.length() > 0) {
+                    reversedWordsStringStore.add(stringStoreIndex, new StringBuilder().
+                            append(reversedWordsStringStore.get(stringStoreIndex - 1).substring(0, wordIndexStart)).
+                            append(stringBuilder.reverse().toString()).toString());
+                    stringBuilder.setLength(0);
+                    stringStoreIndex++;
+                }
+            }
+            characterIndex++;
+        }
+
+        return reversedWordsStringStore.get(reversedWordsStringStore.size() - 1);
     }
 }
---
+++ test/main/java/problems/impl/ReverseWordsInStringImplTest.java
@@ -0,0 +1,17 @@
+package main.java.problems.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseWordsInStringImplTest {
+    private final ReverseWordsInStringImpl reverseWordsInString = new ReverseWordsInStringImpl();
+
+    @Test
+    public void testExpected() {
+        final String testString = "a fox jumped over a log";
+        final String expectedReverseString = "a xof depmuj revo a gol";
+        final String result = reverseWordsInString.reverseWordsInString(testString);
+        Assert.assertEquals(expectedReverseString, result);
+    }
+
+}
