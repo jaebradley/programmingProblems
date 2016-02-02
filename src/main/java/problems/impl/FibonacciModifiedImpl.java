79ebc8a871580a0b4fe31c5f3797f3436065236c
Jae Bradley <jae.b.bradley@gmail.com>
2016-02-01 21:35:22 -0500

    Added test

--- src/main/java/problems/impl/FibonacciModifiedImpl.java
+++ src/main/java/problems/impl/FibonacciModifiedImpl.java
@@ -14,27 +14,64 @@ import java.math.BigInteger;
 
  Given three integers A, B and N, such that the first two terms of the series (1st and 2nd terms)
  are A and B respectively, compute the Nth term of the series.
+
+ Input Constraints
+ 0 <= A,B <= 2
+ 3 <= N <= 20
  */
 
 public class FibonacciModifiedImpl implements FibonacciModified {
 
     @Override
     public BigInteger calculateNextTerm(final BigInteger currentTerm, final BigInteger lastTerm) {
+        if (-1 == lastTerm.compareTo(BigInteger.ZERO) ) {
+            throw new IllegalArgumentException("last term must be non-negative");
+        }
+
+        if (-1 == currentTerm.compareTo(lastTerm)) {
+            throw new IllegalArgumentException("current term must be greater than or equal to last term");
+        }
+
         final BigInteger squaredTerm = new BigDecimal(Math.pow(currentTerm.doubleValue(), 2)).toBigInteger();
         return squaredTerm.add(lastTerm);
     }
 
     @Override
     public BigInteger calculateNthTerm(final BigInteger firstTerm, final BigInteger secondTerm, final int n) {
-        int counter = 0;
-        BigInteger currentTerm = secondTerm;
-        BigInteger lastTerm = firstTerm;
-        while (counter < n) {
-            BigInteger nextTerm = calculateNextTerm(currentTerm, lastTerm);
-            lastTerm = currentTerm;
-            currentTerm = nextTerm;
+        if (-1 == firstTerm.compareTo(BigInteger.ZERO) ) {
+            throw new IllegalArgumentException("first term must be non-negative");
         }
 
-        return currentTerm;
+        if (-1 == secondTerm.compareTo(firstTerm)) {
+            throw new IllegalArgumentException("second term must be greater than or equal to last term");
+        }
+
+        if (n < 1) {
+            throw new IllegalArgumentException("number of terms must be greater than 0");
+        }
+
+        switch (n) {
+            case 1: {
+                return firstTerm;
+            }
+
+            case 2: {
+                return secondTerm;
+            }
+
+            default: {
+                int counter = 2;
+                BigInteger currentTerm = secondTerm;
+                BigInteger lastTerm = firstTerm;
+                while (counter < n) {
+                    BigInteger nextTerm = calculateNextTerm(currentTerm, lastTerm);
+                    lastTerm = currentTerm;
+                    currentTerm = nextTerm;
+                    counter++;
+                }
+
+                return currentTerm;
+            }
+        }
     }
 }
