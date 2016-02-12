626632bf9a7acd7a07354260bd667a439c530db6
jbradley <jbradley@hubspot.com>
2016-02-12 09:32:44 -0500

    Refactored logic

--- src/main/java/problems/impl/HappyNumberCounter.java
+++ src/main/java/problems/impl/HappyNumberCounter.java
@@ -29,33 +29,46 @@ public class HappyNumberCounter {
     long count = 0;
     for (long index = startInclusive; index <= endInclusive; index++) {
       long number = index;
-      final List<Long> numbers = new ArrayList<>();
-      Boolean isHappyNumber = happyNumbers.get(number);
-      long lastDigit = getLastDigit(number);
-      if (isHappyNumber != null) {
+
+      // case 1: already been cached
+      // case 2: has not been cached
+      // case 3: has not been cached but starts with 1 or 4
+      // case 4: has not been cached and does not start with 1 or 4
+
+      Boolean isHappy = happyNumbers.get(index);
+      if (isHappy == Boolean.TRUE) {
+        count++;
+      } else if (isHappy == Boolean.FALSE) {
+        continue;
+      } else if (isHappy == null) {
+        long lastDigit = getLastDigit(number);
         if (lastDigit == 1) {
-          count += 1;
-        }
-      } else {
-        while (lastDigit != 1 && lastDigit != 4 && isHappyNumber == null) {
-          numbers.add(number);
-          number = digitSquareSumCalculator(number);
-          isHappyNumber = happyNumbers.get(number);
-          lastDigit = getLastDigit(number);
+          happyNumbers.put(number, Boolean.TRUE);
+          count++;
         }
 
-        if (isHappyNumber) {
-          for (final Long num : numbers) {
-            happyNumbers.put(num, true);
+        else if (lastDigit == 4) {
+          happyNumbers.put(number, Boolean.FALSE);
+        }
+
+        else {
+          final List<Long> numbers = new ArrayList<>();
+          while (lastDigit!= 1 && lastDigit!= 4 && isHappy == null) {
+            numbers.add(number);
+            number = digitSquareSumCalculator(number);
+            isHappy= happyNumbers.get(number);
+            lastDigit = getLastDigit(number);
           }
-          count += 1;
-        } else {
+
           for (final Long num : numbers) {
-            happyNumbers.put(num, false);
+            happyNumbers.put(num, isHappy);
+          }
+
+          if (isHappy) {
+            count++;
           }
         }
       }
-    }
-    return count;
   }
+    return count;
 }
