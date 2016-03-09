36cd724ee47853262b1b2194338addb9d225808c
jbradley <jbradley@hubspot.com>
2016-03-09 10:11:09 -0500

    Vowel order documentation

--- src/main/java/problems/impl/VowelOrderValidator.java
+++ src/main/java/problems/impl/VowelOrderValidator.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * Validate that a string's vowels are ordered in alphabetical order (A before E before I before O before U before Y)
+ */
+
 public class VowelOrderValidator {
     private static final List<Character> VOWELS = new ArrayList<>(
             Arrays.asList('A', 'E', 'I', 'O', 'U', 'Y')
