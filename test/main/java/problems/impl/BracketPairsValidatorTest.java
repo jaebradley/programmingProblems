aee8a03084a94853dd89eee012dcea8dba4b666a
jbradley <jbradley@hubspot.com>
2016-03-14 09:29:25 -0400

    An improved bracket pairs test

--- test/main/java/problems/impl/BracketPairsValidatorTest.java
+++ test/main/java/problems/impl/BracketPairsValidatorTest.java
@@ -1,13 +1,44 @@
 package problems.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class BracketPairsValidatorTest {
 
   @Test
-  public void testExpected() {
-    Assert.assertFalse(BracketPairsValidator.validBracketPairs("A[B{C}(D)"));
+  public void testBracketValidation() {
+    final List<String> testStrings = new ArrayList<>();
+    testStrings.add("[]");
+    testStrings.add("{}");
+    testStrings.add("()");
+    testStrings.add("[{()}]");
+    testStrings.add("({[]})");
+    testStrings.add("{([])}");
+    testStrings.add("{}[]()");
+    testStrings.add("foo");
+    for (final String testString : testStrings) {
+      Assert.assertTrue(BracketPairsValidator.validBracketPairs(testString));
+    }
+  }
+
+  @Test
+  public void testBracketInvalidation() {
+    final List<String> testStrings = new ArrayList<>();
+    testStrings.add("[");
+    testStrings.add("{");
+    testStrings.add("(");
+    testStrings.add("[()}]");
+    testStrings.add("([]})");
+    testStrings.add("{[}]");
+    testStrings.add("}](");
+    testStrings.add("{}[](");
+    testStrings.add("{f}[o](o");
+    for (final String testString : testStrings) {
+      Assert.assertFalse(BracketPairsValidator.validBracketPairs(testString));
+    }
   }
 
 }
