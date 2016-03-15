db67465e4b42950c1d1a570602ac11d988fdb8df
jbradley <jbradley@hubspot.com>
2016-03-15 18:21:23 -0400

    Added test

---
+++ test/main/java/problems/impl/SplitIntegerGeneratorImplTest.java
@@ -0,0 +1,42 @@
+package problems.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import problems.interfaces.SplitIntegerGenerator;
+import problems.utils.SplitInteger;
+
+public class SplitIntegerGeneratorImplTest {
+  private final SplitIntegerGenerator splitIntegerGenerator = new SplitIntegerGeneratorImpl();
+
+  @Test
+  public void testExpected() {
+    try {
+      splitIntegerGenerator.generateSplitInteger(-1);
+      Assert.fail();
+    } catch (IllegalArgumentException e) {
+      // expected
+    }
+
+    try {
+      splitIntegerGenerator.generateSplitInteger(0);
+      Assert.fail();
+    } catch (IllegalArgumentException e) {
+      // expected
+    }
+
+    final SplitInteger oddDigitCountSplitInteger = splitIntegerGenerator.generateSplitInteger(123456789);
+    Assert.assertEquals(oddDigitCountSplitInteger.getLeftDigits(), new ArrayList(Arrays.asList(1, 2, 3, 4)));
+    Assert.assertEquals(oddDigitCountSplitInteger.getRightDigits(), new ArrayList(Arrays.asList(5, 6, 7, 8, 9)));
+
+    final SplitInteger evenDigitCountSplitInteger = splitIntegerGenerator.generateSplitInteger(12345678);
+    Assert.assertEquals(evenDigitCountSplitInteger.getLeftDigits(), new ArrayList(Arrays.asList(1, 2, 3, 4)));
+    Assert.assertEquals(evenDigitCountSplitInteger.getRightDigits(), new ArrayList(Arrays.asList(5, 6, 7, 8)));
+  }
+
+}
