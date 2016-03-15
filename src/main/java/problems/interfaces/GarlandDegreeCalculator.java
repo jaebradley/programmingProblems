fca70725e110a088dba7ee5643f3b8d39b6acdfe
jbradley <jbradley@hubspot.com>
2016-03-15 17:42:06 -0400

    Removed Garland degree calculator

--- src/main/java/problems/interfaces/GarlandDegreeCalculator.java
+++
@@ -1,7 +0,0 @@
-package problems.interfaces;
-
-public interface GarlandDegreeCalculator {
-    int calculateGarlandDegree(final String candidate);
-
-    int subsetIndexIdentifier(final char[] chars, final char[] candidateChars);
-}
