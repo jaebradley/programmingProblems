117f2c88a2e92ffeac0139fa27fa5bcd27fc9ce3
jbradley <jbradley@hubspot.com>
2016-03-18 19:38:22 -0400

    Updated test

--- src/test/java/problems/impl/ISBNValidatorImplTest.java
+++ src/test/java/problems/impl/ISBNValidatorImplTest.java
@@ -1,12 +1,27 @@
 package problems.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import problems.interfaces.ISBNValidator;
+import problems.interfaces.InverseMultiplierListProductSumGenerator;
 
 public class ISBNValidatorImplTest {
-  private final ISBNValidator isbnValidator = new ISBNValidatorImpl();
+  private final List<Integer> testValues1 = new ArrayList<>(Arrays.asList(1));
+  private final InverseMultiplierListProductSumGenerator inverseMultiplierListProductSumGenerator = new InverseMultiplierListProductSumGenerator() {
+    @Override public long generateInverseMultiplierListProductSum(final List<Integer> values) {
+      if (values == testValues1) {
+        return 11;
+      } else {
+        return 10;
+      }
+    }
+  };
+  private final ISBNValidator isbnValidator = new ISBNValidatorImpl(inverseMultiplierListProductSumGenerator);
 
   @Test
   public void testValidateISBN() {
@@ -24,7 +39,8 @@ public class ISBNValidatorImplTest {
       // expected
     }
 
-    Assert.assertFalse(isbnValidator.validateISBN(1000000000));
+    Assert.assertFalse(isbnValidator.validateISBN(1));
+    Assert.assertTrue(isbnValidator.validateISBN(2));
   }
 
 }
