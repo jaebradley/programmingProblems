45795f90579631e7e81798107be45eba4afde8b3
jbradley <jbradley@hubspot.com>
2016-03-23 18:42:33 -0400

    Java doc

--- src/main/java/problems/impl/DigitCollectionConverterImpl.java
+++ src/main/java/problems/impl/DigitCollectionConverterImpl.java
@@ -5,7 +5,15 @@ import java.util.Collection;
 import problems.interfaces.DigitCollectionConverter;
 
 public class DigitCollectionConverterImpl implements DigitCollectionConverter {
-  @Override public int convertDigitCollectionToInteger(final Collection<Integer> digits) {
+
+  /**
+   * Given a collection of integers, return the numerical value if these integers were concatenated.
+   *
+   * @param digits collection of integers
+   * @return integer, representing the numerical value of the concatenated input collection
+   */
+  @Override
+  public int convertDigitCollectionToInteger(final Collection<Integer> digits) {
     if (digits.isEmpty()) {
       throw new IllegalArgumentException("empty input collection");
     }
