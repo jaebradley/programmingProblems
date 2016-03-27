1a045cb19e4370fd331a92953e3d293aa401db14
jbradley <jbradley@hubspot.com>
2016-03-26 23:46:08 -0400

    Added javadoc for DivisorsCalculatorImpl'

--- src/main/java/problems/impl/DivisorsCalculatorImpl.java
+++ src/main/java/problems/impl/DivisorsCalculatorImpl.java
@@ -6,7 +6,20 @@ import java.util.Set;
 import problems.interfaces.DivisorsCalculator;
 
 public class DivisorsCalculatorImpl implements DivisorsCalculator {
-  @Override public Set<Integer> calculateDivisors(final long positiveInteger) {
+
+  /**
+   * Calculates divisors for a given positive integer.
+   *
+   * Divides the input value by each number between 1 and the square root of the input value, rounded up.
+   * If the candidate divisor returns a remainder of zero, then add the candidate divisor and it's division value
+   * to the Set of values to return.
+   *
+   * @param positiveInteger value to calculate divisors from
+   * @return a Set of numbers that contains the divisors of the input value
+   */
+
+  @Override
+  public Set<Integer> calculateDivisors(final long positiveInteger) {
     if (positiveInteger < 1) {
       throw new IllegalArgumentException("input value must be positive");
     }
