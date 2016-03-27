3a79bd96e080ec00a95cd17120e7167fec69e26d
jbradley <jbradley@hubspot.com>
2016-03-26 23:26:58 -0400

    removed static method

--- src/test/java/problems/utils/NumberUtilTest.java
+++ src/test/java/problems/utils/NumberUtilTest.java
@@ -2,8 +2,6 @@ package problems.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -35,30 +33,6 @@ public class NumberUtilTest {
     Assert.assertEquals(NumberUtil.positiveIntegerDigits(1), new ArrayList<>(Arrays.asList(1)));
     Assert.assertEquals(NumberUtil.positiveIntegerDigits(10), new ArrayList<>(Arrays.asList(1, 0)));
     Assert.assertEquals(NumberUtil.positiveIntegerDigits(100), new ArrayList<>(Arrays.asList(1, 0, 0)));
-  }
-
-  @Test
-  public void testDivisorsForPositiveIntegers() {
-    try {
-      NumberUtil.getDivisorsForPositiveInteger(-1);
-      Assert.fail();
-    } catch (RuntimeException e) {
-      // expected
-    }
-
-    try {
-      NumberUtil.getDivisorsForPositiveInteger(0);
-      Assert.fail();
-    } catch (RuntimeException e) {
-      // expected
-    }
-
-    Set<Integer> oneDivisors = new HashSet<>(Arrays.asList(1));
-    Set<Integer> twoDivisors = new HashSet<>(Arrays.asList(1, 2));
-    Set<Integer> tenDivisors = new HashSet<>(Arrays.asList(1, 2, 5, 10));
-    Assert.assertEquals(NumberUtil.getDivisorsForPositiveInteger(1), oneDivisors);
-    Assert.assertEquals(NumberUtil.getDivisorsForPositiveInteger(2), twoDivisors);
-    Assert.assertEquals(NumberUtil.getDivisorsForPositiveInteger(10), tenDivisors);
   }
 
 }
