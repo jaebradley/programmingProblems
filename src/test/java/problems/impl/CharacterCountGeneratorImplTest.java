42765d268884019cdd2f101bbd01dbe3c854770a
jbradley <jbradley@hubspot.com>
2016-04-01 01:47:15 -0400

    Added alphabetic count test cases

--- src/test/java/problems/impl/CharacterCountGeneratorImplTest.java
+++ src/test/java/problems/impl/CharacterCountGeneratorImplTest.java
@@ -14,15 +14,34 @@ public class CharacterCountGeneratorImplTest {
   @Test
   public void itShouldIncrementCharacterCounts() {
     final Map<Character, Integer> characterCounts1 = new HashMap<>();
-    characterCountGenerator.incrementCharacterCount(characterCounts1, 'A');
+    characterCountGenerator.incrementCharacterCount(characterCounts1, '1');
+    final Map<Character, Integer> expectedCharacterCounts1 = new HashMap<>();
+    expectedCharacterCounts1.put('1', 1);
+    Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
+    characterCountGenerator.incrementCharacterCount(characterCounts1, '1');
+    expectedCharacterCounts1.put('1', 2);
+    Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
+    characterCountGenerator.incrementCharacterCount(characterCounts1, ' ');
+    expectedCharacterCounts1.put(' ', 1);
+    Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
+  }
+
+  @Test
+  public void itShouldIncrementAlphabeticCharacterCounts() {
+    final Map<Character, Integer> characterCounts1 = new HashMap<>();
+    characterCountGenerator.incrementAlphabeticCharacterCount(characterCounts1, 'A');
     final Map<Character, Integer> expectedCharacterCounts1 = new HashMap<>();
     expectedCharacterCounts1.put('A', 1);
     Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
-    characterCountGenerator.incrementCharacterCount(characterCounts1, 'A');
+    characterCountGenerator.incrementAlphabeticCharacterCount(characterCounts1, 'A');
     expectedCharacterCounts1.put('A', 2);
     Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
-    characterCountGenerator.incrementCharacterCount(characterCounts1, 'B');
+    characterCountGenerator.incrementAlphabeticCharacterCount(characterCounts1, 'B');
     expectedCharacterCounts1.put('B', 1);
+    Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
+    characterCountGenerator.incrementAlphabeticCharacterCount(characterCounts1, ' ');
+    Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
+    characterCountGenerator.incrementAlphabeticCharacterCount(characterCounts1, '1');
     Assert.assertEquals(characterCounts1, expectedCharacterCounts1);
   }
 
@@ -36,6 +55,17 @@ public class CharacterCountGeneratorImplTest {
     Assert.assertEquals(characterCountGenerator.generateCharacterCount("AC BC"), characterCounts);
     final Map<Character, Integer> emptyCharacterCounts = new HashMap<>();
     Assert.assertEquals(characterCountGenerator.generateCharacterCount(""), emptyCharacterCounts);
+  }
+
+  @Test
+  public void itShouldGenerateAlphabeticCharacterCounts() {
+    final Map<Character, Integer> characterCounts = new HashMap<>();
+    characterCounts.put('A', 1);
+    characterCounts.put('B', 1);
+    characterCounts.put('C', 2);
+    Assert.assertEquals(characterCountGenerator.generateAlphabeticCharacterCount("AC BC 1"), characterCounts);
+    final Map<Character, Integer> emptyCharacterCounts = new HashMap<>();
+    Assert.assertEquals(characterCountGenerator.generateAlphabeticCharacterCount(""), emptyCharacterCounts);
   }
 
 }
