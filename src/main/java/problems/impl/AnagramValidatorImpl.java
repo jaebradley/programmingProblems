b21e59b760e26487800d183af09d1f63319592e4
jbradley <jbradley@hubspot.com>
2016-03-31 20:42:37 -0400

    Added anagram validator interface and implementation

---
+++ src/main/java/problems/impl/AnagramValidatorImpl.java
@@ -0,0 +1,10 @@
+package problems.impl;
+
+import problems.interfaces.AnagramValidator;
+
+public class AnagramValidatorImpl implements AnagramValidator {
+  @Override
+  public boolean areAnagrams(final String value1, final String value2) {
+    return false;
+  }
+}
