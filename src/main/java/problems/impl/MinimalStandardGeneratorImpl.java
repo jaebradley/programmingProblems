5a683b2937aae4b71931bf5e341521b4df6f2627
jaebradley <jae.b.bradley@gmail.com>
2016-06-02 22:54:39 -0400

    minimal standard generator implementation

---
+++ src/main/java/problems/impl/MinimalStandardGeneratorImpl.java
@@ -0,0 +1,20 @@
+package problems.impl;
+
+import problems.interfaces.MinimalStandardGenerator;
+
+public class MinimalStandardGeneratorImpl implements MinimalStandardGenerator {
+  private static final int MODULUS = 2147483647;
+  private static final int MULTIPLIER = 48271;
+  private static final double Q = MODULUS / MULTIPLIER;
+  private static final int R = MODULUS % MULTIPLIER;
+  private static int SEED = 1;
+
+  @Override
+  public int generateRandomNumber() {
+    SEED = Double.valueOf(MULTIPLIER * (SEED / Q) - R * (SEED / Q)).intValue();
+    if (SEED < 0) {
+      return SEED += MODULUS;
+    }
+    return SEED;
+  }
+}
---
+++ src/main/java/problems/interfaces/MinimalStandardGenerator.java
@@ -0,0 +1,5 @@
+package problems.interfaces;
+
+public interface MinimalStandardGenerator {
+  int generateRandomNumber();
+}
