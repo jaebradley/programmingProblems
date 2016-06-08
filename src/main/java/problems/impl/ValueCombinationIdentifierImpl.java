6e95ca67104653c639aa4e1d2d166f7b323c1d87
jaebradley <jae.b.bradley@gmail.com>
2016-06-07 23:23:03 -0400

    added java doc

--- src/main/java/problems/impl/ValueCombinationIdentifierImpl.java
+++ src/main/java/problems/impl/ValueCombinationIdentifierImpl.java
@@ -8,7 +8,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Given a list of values and some target, find the first combination of values (if they exist) that sums to the target value
+ *
+ * Inspired by this problem: https://www.hackerrank.com/challenges/icecream-parlor
+ */
+
 public class ValueCombinationIdentifierImpl implements ValueCombinationIdentifier {
+
+  /**
+   * For a given target value, find the pair of values (if they exist) in the inputted List of values that sum to the
+   * target value. If no pair of values is found, a NoValueCombinationIdentifiedException is thrown.
+   * 
+   * @param target value that two candidate values need to sum to
+   * @param values List of candidate values
+   * @return TwoValueCombination object that contains the pair of values that sum to the given target value
+   * @throws NoValueCombinationIdentifiedException if no pair of values can be identified
+   */
   @Override
   public TwoValueCombination identifyTwoValuesThatSumToTarget(final long target, final List<Long> values) throws NoValueCombinationIdentifiedException {
     if (null == values) {
