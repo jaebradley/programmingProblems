10e56cee5fa9a8046b77cc3c073e341d35018792
jaebradley <jae.b.bradley@gmail.com>
2016-07-04 22:58:38 -0400

    refactoring

--- src/main/java/problems/interfaces/AdjacentDuplicateCharactersRemover.java
+++ src/main/java/problems/interfaces/AdjacentDuplicateCharactersRemover.java
@@ -1,10 +1,5 @@
 package problems.interfaces;
 
-import java.util.List;
-import java.util.Stack;
-
 public interface AdjacentDuplicateCharactersRemover {
   String removeAdjacentDuplicateCharacters(final String candidate);
-
-  List<Character> filterAdjacentDuplicateCharacters(final Stack<Character> characters);
 }
--- src/test/java/problems/impl/AdjacentDuplicateCharactersRemoverImplTest.java
+++ src/test/java/problems/impl/AdjacentDuplicateCharactersRemoverImplTest.java
@@ -3,34 +3,11 @@ package problems.impl;
 import org.junit.Test;
 import problems.interfaces.AdjacentDuplicateCharactersRemover;
 
-import java.util.Arrays;
-import java.util.Stack;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AdjacentDuplicateCharactersRemoverImplTest {
 
   private final AdjacentDuplicateCharactersRemover adjacentDuplicateCharactersRemover = new AdjacentDuplicateCharactersRemoverImpl();
-  private final Stack<Character> emptyStack = new Stack<>();
-
-  @Test
-  public void testFilterAdjacentDuplicateCharacters() {
-    assertEquals(adjacentDuplicateCharactersRemover.filterAdjacentDuplicateCharacters(emptyStack), emptyStack);
-
-    final Stack<Character> singleElementStack = new Stack<>();
-    singleElementStack.add('A');
-    assertEquals(adjacentDuplicateCharactersRemover.filterAdjacentDuplicateCharacters(singleElementStack), singleElementStack);
-
-    final Stack<Character> uniqueElementsStack = new Stack<>();
-    uniqueElementsStack.addAll(Arrays.asList('A', 'B', 'C', 'D'));
-    final Stack<Character> expectedUniqueElementsStack = new Stack<>();
-    expectedUniqueElementsStack.addAll(Arrays.asList('D', 'C', 'B', 'A'));
-    assertEquals(adjacentDuplicateCharactersRemover.filterAdjacentDuplicateCharacters(uniqueElementsStack), expectedUniqueElementsStack);
-
-    final Stack<Character> allDuplicateElementsStack = new Stack<>();
-    allDuplicateElementsStack.addAll(Arrays.asList('B', 'A', 'A', 'B'));
-    assertEquals(adjacentDuplicateCharactersRemover.filterAdjacentDuplicateCharacters(allDuplicateElementsStack), emptyStack);
-  }
 
   @Test
   public void testRemoveAdjacentDuplicateCharacters() {
