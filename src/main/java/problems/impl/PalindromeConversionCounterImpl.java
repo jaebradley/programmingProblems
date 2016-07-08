b7a965264e0ccf21c9c06de702459d159c5b2e68
jaebradley <jae.b.bradley@gmail.com>
2016-07-07 23:57:16 -0400

    added javadoc

--- src/main/java/problems/impl/PalindromeConversionCounterImpl.java
+++ src/main/java/problems/impl/PalindromeConversionCounterImpl.java
@@ -2,7 +2,20 @@ package problems.impl;
 
 import problems.interfaces.PalindromeConversionCounter;
 
+/**
+ * Inspired by this HackerRank challenge https://www.hackerrank.com/challenges/the-love-letter-mystery
+ *
+ * Basically, count the minimum number of operations to convert a String to a palindrome. An additional rule is that
+ * each operation must reduce the "value" of the character. So, can convert "d" to "c" but not "c" to "d".
+ */
 public class PalindromeConversionCounterImpl implements PalindromeConversionCounter {
+  /**
+   * Counts the number of character replacements necessary to convert the input String to a palindrome. Characters must
+   * be replaced by a character that is of a lesser "value" of the current character. So "c" can be replaced with "b"
+   * but not "d".
+   * @param s String to convert
+   * @return count of character replacements
+   */
   @Override
   public int count(final String s) {
     int count = 0;
