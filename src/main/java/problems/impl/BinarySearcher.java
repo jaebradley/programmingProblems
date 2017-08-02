9fd7759d951853ffa3fe6b7abee42a19320f42e9
jaebradley <jae.b.bradley@gmail.com>
2017-08-02 10:04:08 -0400

    update binary search implementation

--- src/main/java/problems/impl/BinarySearcher.java
+++ src/main/java/problems/impl/BinarySearcher.java
@@ -10,36 +10,25 @@ public class BinarySearcher {
      * @return the index of the target value, if it exists. If the target value does not exist, a NoSuchElementException is thrown.
      */
     public static int search(int[] values, int target) {
-        return searchSubArray(values, 0, values.length - 1, target);
-    }
-
-    private static int searchSubArray(int[] values, int leftIndex, int rightIndex, int target) {
-        if (values.length == 0 || leftIndex < 0 || rightIndex < 0) {
+        if (values.length == 0) {
             throw new NoSuchElementException(String.format("Unable to find target: {} in values", target));
         }
 
-        if (target == values[leftIndex]) {
-            return leftIndex;
+        int leftIndex = 0;
+        int rightIndex = values.length - 1;
+        while (leftIndex < rightIndex) {
+            int middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+            int middleValue = values[middleIndex];
+
+            if (target < middleValue) {
+                rightIndex = middleIndex;
+            } else if (target > middleValue) {
+                leftIndex = middleIndex + 1;
+            } else {
+                return middleIndex;
+            }
         }
 
-        if (target == values[rightIndex]) {
-            return rightIndex;
-        }
-
-        int middleIndex = Double.valueOf(Math.floor((rightIndex - leftIndex) / 2)).intValue();
-
-        int middleValue = values[middleIndex];
-
-        if (target < middleValue) {
-            return searchSubArray(values, leftIndex + 1, middleIndex - 1, target);
-        }
-
-        else if (target > middleValue) {
-            return searchSubArray(values, middleIndex + 1, rightIndex - 1, target);
-        }
-
-        else {
-            return middleIndex;
-        }
+        return leftIndex;
     }
 }
